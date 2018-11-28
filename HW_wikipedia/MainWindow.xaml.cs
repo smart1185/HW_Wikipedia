06f12e84059b3f427cc47c381e43b181be00eff2
HP <HP@USER>
2018-11-28 11:36:50 +0600

    Добавьте файлы проекта.

---
+++ HW_wikipedia/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using HW_wikipedia.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW_wikipedia
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Frame mf { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            mf = MainFrame;
+
+            MainFrame.Source = new Uri("Pages/Page1.xaml", UriKind.RelativeOrAbsolute);
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            RubPage rp = new RubPage();
+            MainFrame.NavigationService.Navigate(rp);
+        }
+    }
+}
